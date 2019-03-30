a4aac48cfe54fba57b037e2c7f81bc7ca3db6c86
Piyush Agarwal <piyushagarwal.0108@gmail.com>
2019-03-30 18:14:03 +0530

    new game--relationship calculator

---
+++ flames.py
@@ -0,0 +1,38 @@
+#flames = {"f":"friends","l":"love","a":"affectionate","m":"marriage","e":"enemies","s":"sister"}
+print("This is the Realtion Calculator")
+import sys
+flames = ["friends","love","affectionate","marriage","enemies","siblings"]
+boy = list(input("enter name of boy: ").lower())
+girl = list(input("enter name of girl: ").lower())
+#print(boy,girl)
+try:
+    common = [i for i in boy if i in girl]
+    common = set(common)
+    #print(common)
+except:
+    print("huh...you are just friends")
+    sys.exit()
+for i in common:
+    boy.remove(i)
+    girl.remove(i)
+#print(boy)
+#print(girl)
+total_similar = len(boy)+len(girl)
+
+while(len(flames)!=1):
+    count = (total_similar%len(flames))-1
+    if count>0:
+        right = flames[count+1:]
+        left = flames[:count]
+        flames = right+left
+    else:
+        flames = flames[:len(flames)-1]
+
+print(flames[0])
+        
+        
+    
+    
+    
+    
+    
